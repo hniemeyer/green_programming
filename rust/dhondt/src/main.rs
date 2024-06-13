6a5b58b285e9481930fd7c241563fa7eb8b126dc
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2024-06-13 10:18:08 +0200

    renaming

--- rust/dhondt/src/main.rs
+++ rust/dhondt/src/main.rs
@@ -13,14 +13,14 @@ fn main() {
     const NUMBER_OF_SEATS: usize = 10;
     let election = generate_data::generate_parties();
 
-    let it = (1..NUMBER_OF_SEATS + 1).cartesian_product(election.iter());
+    let cart_product = (1..NUMBER_OF_SEATS + 1).cartesian_product(election.iter());
 
-    let it2 = it.map(|(i, c)| DHondtIntermediate {
+    let parties_and_divisor_results = cart_product.map(|(i, c)| DHondtIntermediate {
         party: c.party.clone(),
         votes_per_divisor: c.votes as f64 / i as f64,
     });
 
-    let it3 = it2
+    let seats_in_parliament = parties_and_divisor_results
         .sorted_by(|a, b| {
             b.votes_per_divisor
                 .partial_cmp(&a.votes_per_divisor)
@@ -28,10 +28,10 @@ fn main() {
         })
         .take(NUMBER_OF_SEATS);
 
-    let mut final_results: HashMap<String, usize> = HashMap::new();
-    for elem in it3 {
-        *final_results.entry(elem.party).or_default() += 1;
+    let mut final_results_aggregated: HashMap<String, usize> = HashMap::new();
+    for elem in seats_in_parliament {
+        *final_results_aggregated.entry(elem.party).or_default() += 1;
     }
 
-    println!("{:?}", final_results);
+    println!("{:?}", final_results_aggregated);
 }
