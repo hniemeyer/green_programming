e80c9f5aa1bf702e91717c2e0ea36a64a22068c9
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2024-06-13 10:07:59 +0200

    correct calculation

--- rust/dhondt/src/main.rs
+++ rust/dhondt/src/main.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 
 struct VotingResults {
     party: String,
@@ -28,19 +29,24 @@ fn main() {
     ];
 
     let it = (1..NUMBER_OF_SEATS + 1).cartesian_product(election.iter());
+
     let it2 = it.map(|(i, c)| DHondtIntermediate {
         party: c.party.clone(),
         votes_per_divisor: c.votes as f64 / i as f64,
     });
-    let it3 = it2.sorted_by(|a, b| {
-        b.votes_per_divisor
-            .partial_cmp(&a.votes_per_divisor)
-            .unwrap()
-    });
-    println!(
-        "{}",
-        it3.take(NUMBER_OF_SEATS)
-            .filter(|elem| elem.party == "C")
-            .count()
-    );
+
+    let it3 = it2
+        .sorted_by(|a, b| {
+            b.votes_per_divisor
+                .partial_cmp(&a.votes_per_divisor)
+                .unwrap()
+        })
+        .take(NUMBER_OF_SEATS);
+
+    let mut final_results: HashMap<String, usize> = HashMap::new();
+    for elem in it3 {
+        *final_results.entry(elem.party).or_default() += 1;
+    }
+
+    println!("{:?}", final_results);
 }
