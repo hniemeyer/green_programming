78f3eb0e9ece75eba6232f73eeec749358c9ffec
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2024-06-23 08:50:30 +0200

    Dhondt in Typescript (#1)
    * dhondt in typescript
    
    stub
    
    * random parties
    
    for later we need the possibility to generate a large number of random results.
    
    * stub for readme
    
    * typescript playground

---
+++ typescript/dhondt.ts
@@ -0,0 +1,73 @@
+interface Party {
+    name: string;
+    votes: number;
+    seats: number;
+}
+
+interface Quotient {
+    partyIndex: number;
+    value: number;
+}
+
+
+function getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomName(length: number): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let name = '';
+    for (let i = 0; i < length; i++) {
+        name += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return name;
+}
+
+function generateRandomParties(numParties: number, maxVotes: number): Party[] {
+    const parties: Party[] = [];
+    for (let i = 0; i < numParties; i++) {
+        const nameLength = getRandomInt(1, 10);
+        const partyName = getRandomName(nameLength);
+        const votes = getRandomInt(0, maxVotes);
+        parties.push({ name: partyName, votes: votes, seats: 0 });
+    }
+    return parties;
+}
+
+function dHondt(parties: Party[], totalSeats: number): Party[] {
+    // Initialize seats for each party to 0
+    parties.forEach(party => {
+        party.seats = 0;
+    });
+
+    let quotients: Quotient[] = [];
+
+    // Generate all quotients
+    for (let i = 0; i < parties.length; i++) {
+        for (let j = 1; j <= totalSeats; j++) {
+            quotients.push({
+                partyIndex: i,
+                value: parties[i].votes / j
+            });
+        }
+    }
+
+    // Sort quotients in descending order
+    quotients.sort((a, b) => b.value - a.value);
+
+    // Allocate seats based on the highest quotients
+    for (let i = 0; i < totalSeats; i++) {
+        let partyIndex = quotients[i].partyIndex;
+        parties[partyIndex].seats++;
+    }
+
+    return parties;
+}
+
+const numParties = 5;
+const maxVotes = 1000;
+const parties: Party[] = generateRandomParties(numParties, maxVotes);
+
+const totalSeats = 10;
+const result = dHondt(parties, totalSeats);
+console.log(result);
