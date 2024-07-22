76494b9418681faec88b65ad84d33f1f631c8e06
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2024-07-22 14:38:30 +0200

    prototype measurement

--- measurement.py
+++ measurement.py
@@ -1,13 +1,26 @@
 from codecarbon import EmissionsTracker
 import numpy as np
 
-tracker = EmissionsTracker()
-tracker.start()
-try:
+with EmissionsTracker(project_name="large") as tracker:
      # Compute intensive code goes here
-     for idx in range(5):
+     for idx in range(10):
+          print(idx)
           a = np.random.randn(10000, 10000)
           b = np.random.randn(10000, 10000)
           c = a*b
-finally:
-     tracker.stop()
+
+with EmissionsTracker(project_name="medium") as tracker:
+     # Compute intensive code goes here
+     for idx in range(10):
+          print(idx)
+          a = np.random.randn(1000, 1000)
+          b = np.random.randn(1000, 1000)
+          c = a*b
+
+with EmissionsTracker(project_name="small") as tracker:
+     # Compute intensive code goes here
+     for idx in range(10):
+          print(idx)
+          a = np.random.randn(10, 10)
+          b = np.random.randn(10, 10)
+          c = a*b
