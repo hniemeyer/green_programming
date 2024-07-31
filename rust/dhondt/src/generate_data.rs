86ecad0831a55c74c97bc717a53f89cbe9ca76b2
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2024-07-31 22:20:55 +0200

    remove randomness from rust

--- rust/dhondt/src/generate_data.rs
+++ rust/dhondt/src/generate_data.rs
@@ -1,32 +1,11 @@
 use crate::voting::VotingResults;
-use rand::Rng;
-
-fn generate_random_name() -> String {
-    // Define the alphabet
-    let alphabet: Vec<char> = ('a'..='z').collect();
-
-    // Generate a random length between 1 and 10
-    let mut rng = rand::thread_rng();
-    let name_length = rng.gen_range(1..=10);
-
-    // Generate a random name of the determined length
-    let name: String = (0..name_length)
-        .map(|_| {
-            let idx = rng.gen_range(0..alphabet.len());
-            alphabet[idx]
-        })
-        .collect();
-
-    name
-}
 
 pub fn generate_parties(number_of_parties: usize) -> Vec<VotingResults> {
-    let mut rng = rand::thread_rng();
     let mut parties = Vec::new();
-
-    for _ in 0..number_of_parties {
-        let party = generate_random_name();
-        let votes = rng.gen_range(1..=1000);
+    let alphabet: Vec<char> = ('a'..='z').collect();
+    for idx in 0..number_of_parties {
+        let party: String = alphabet[idx].to_string();
+        let votes = (idx+1)*1000;
 
         let voting_results = VotingResults { party, votes };
         parties.push(voting_results);
