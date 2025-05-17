ad338c373c2a2ccaca37a1de2c24a3a64e6f0d4b
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2025-05-17 09:53:38 +0200

    alder32 csharp

---
+++ cpp_on_sea/adler32/csharp/Adler32/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+public class Adler32
+{
+    private const uint MOD_ADLER = 65521;
+
+    public static uint ComputeChecksum(byte[] data)
+    {
+        uint a = 1, b = 0;
+
+        foreach (byte byteValue in data)
+        {
+            a = (a + byteValue) % MOD_ADLER;
+            b = (b + a) % MOD_ADLER;
+        }
+
+        return (b << 16) | a;
+    }
+
+    public static void Main()
+    {
+        byte[] data = { 1, 2, 3, 4, 5 };
+        uint checksum1 = ComputeChecksum(data);
+        Console.WriteLine("Adler-32 Checksumme: " + checksum1);
+    }
+}
---
+++ cpp_on_sea/adler32/csharp/Adler32Linq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+public class Adler32
+{
+    private const uint MOD_ADLER = 65521;
+    public static uint ComputeChecksumWithLinq(byte[] data)
+    {
+        var result = data.Aggregate((a: 1u, b: 0u), (acc, byteValue) =>
+        {
+            acc.a = (acc.a + byteValue) % MOD_ADLER;
+            acc.b = (acc.b + acc.a) % MOD_ADLER;
+            return acc;
+        });
+
+        return (result.b << 16) | result.a;
+    }
+
+    public static void Main()
+    {
+        byte[] data = { 1, 2, 3, 4, 5 };
+        uint checksum1 = ComputeChecksum(data);
+        Console.WriteLine("Adler-32 Checksumme: " + checksum1);
+    }
+}
