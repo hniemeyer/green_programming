497719c7ec42a4fba5d0d699ac6e231da0555c2b
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2025-05-17 09:50:40 +0200

    montecarlo pi csharp

---
+++ cpp_on_sea/monte_carlo_pi/csharp/MonteCarloPi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    public static double montecarlo_pi(int numIterations)
+    {
+        int insideCircle = 0;
+        Random rand = new Random();
+
+        for (int i = 0; i < numIterations; i++)
+        {
+            double x = rand.NextDouble();
+            double y = rand.NextDouble();
+
+            if (x * x + y * y <= 1.0)
+            {
+                insideCircle++;
+            }
+        }
+
+        return 4.0 * insideCircle / (double)numIterations;
+    }
+
+    static void Main()
+    {
+        int numIterations = 10000000;
+        Console.WriteLine($"Estimated Pi: {montecarlo_pi(numIterations)}");
+    }
+}
---
+++ cpp_on_sea/monte_carlo_pi/csharp/MonteCarloPiLinq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    public static double montecarlo_pi_with_linq(int numIterations)
+    {
+        Random rand = new Random();
+        var points = Enumerable.Range(0, numIterations).Select(_ => new { X = rand.NextDouble(), Y = rand.NextDouble() });
+        int insideCircle = points.Count(p => p.X * p.X + p.Y * p.Y <= 1.0);
+
+        return 4.0 * insideCircle / (double)numIterations;
+    }
+
+    static void Main()
+    {
+        int numIterations = 10000000;
+        Console.WriteLine($"Estimated Pi: {montecarlo_pi_with_linq(numIterations)}");
+    }
+}
