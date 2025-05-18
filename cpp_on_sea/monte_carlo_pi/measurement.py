7e21dcd50d9b3dc384d4459626ca00ad64a053ce
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2025-05-18 16:48:00 +0200

    wip

--- cpp_on_sea/monte_carlo_pi/measurement.py
+++ cpp_on_sea/monte_carlo_pi/measurement.py
@@ -47,8 +47,9 @@ with EmissionsTracker(project_name="ruby", log_level="error") as tracker:
 with EmissionsTracker(project_name="lua", log_level="error") as tracker:
      os.system("lua ./lua/monte_carlo_pi.lua")
 
-with EmissionsTracker(project_name="elixir", log_level="error") as tracker:
-     os.system("elixir ./elixir/montecarlopi.ex")
+# crashes with memory problems
+# with EmissionsTracker(project_name="elixir", log_level="error") as tracker:
+#      os.system("elixir ./elixir/montecarlopi.ex")
 
 with EmissionsTracker(project_name="java", log_level="error") as tracker:
      os.system("java ./java/MonteCarloPi.java")
