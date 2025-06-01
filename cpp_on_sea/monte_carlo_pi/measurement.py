e8eac9d544fbfb880c3eaac5b5d586be5105805e
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2025-06-01 08:07:31 +0200

    monte carlo pi

--- cpp_on_sea/monte_carlo_pi/measurement.py
+++ cpp_on_sea/monte_carlo_pi/measurement.py
@@ -17,29 +17,17 @@ os.system("dmd -O dlang/montecarlopi.d -of=./dlang/montecarlopi -od=./dlang")
 with EmissionsTracker(project_name="d", log_level="error") as tracker:
      os.system("./dlang/montecarlopi")
 
-os.system("gcc clang/montecarlopi.c -std=c2x -o clang/montecarlopi")
-with EmissionsTracker(project_name="c", log_level="error") as tracker:
-      os.system("./clang/montecarlopi")
-
 os.system("gcc clang/montecarlopi.c -std=c2x -O3 -o clang/montecarlopiopt")
-with EmissionsTracker(project_name="c optimized", log_level="error") as tracker:
+with EmissionsTracker(project_name="c", log_level="error") as tracker:
       os.system("./clang/montecarlopiopt")
 
 os.system("g++-14 ./cpp/montecarlopi_ranges.cpp -std=c++23 -O3 -o ./cpp/rangesopt")
-with EmissionsTracker(project_name="cpp ranges optimized", log_level="error") as tracker:
+with EmissionsTracker(project_name="cpp ranges", log_level="error") as tracker:
      os.system("./cpp/rangesopt")
 
-os.system("g++-14 ./cpp/montecarlopi_ranges.cpp -std=c++23 -o ./cpp/ranges")
-with EmissionsTracker(project_name="cpp ranges", log_level="error") as tracker:
-     os.system("./cpp/ranges")
-
 os.system("g++-14 ./cpp/montecarlopi.cpp -O3 -std=c++23 -o ./cpp/stlopt")
-with EmissionsTracker(project_name="cpp stl optimized", log_level="error") as tracker:
-     os.system("./cpp/stlopt")
-
-os.system("g++-14 ./cpp/montecarlopi.cpp -std=c++23 -o ./cpp/stl")
 with EmissionsTracker(project_name="cpp stl", log_level="error") as tracker:
-     os.system("./cpp/stl")
+     os.system("./cpp/stlopt")
 
 with EmissionsTracker(project_name="ruby", log_level="error") as tracker:
      os.system("ruby ./ruby/montecarlopi.rb")
@@ -54,12 +42,8 @@ with EmissionsTracker(project_name="java", log_level="error") as tracker:
      os.system("java ./java/MonteCarloPi.java")
 
 os.system("cargo build --manifest-path=./rust/pi_calculation/Cargo.toml --release")
-with EmissionsTracker(project_name="rust optimized", log_level="error") as tracker:
-     os.system("cargo run --manifest-path=./rust/pi_calculation/Cargo.toml --release")
-
-os.system("cargo build --manifest-path=./rust/pi_calculation/Cargo.toml")
 with EmissionsTracker(project_name="rust", log_level="error") as tracker:
-     os.system("cargo run --manifest-path=./rust/pi_calculation/Cargo.toml")
+     os.system("cargo run --manifest-path=./rust/pi_calculation/Cargo.toml --release")
 
 os.system("kotlinc ./kotlin/montecarlopi.kt -include-runtime -d ./kotlin/montecarlopi.jar")
 with EmissionsTracker(project_name="kotlin", log_level="error") as tracker:
